646b9a64f7dc263f458ff02578061857ba86d208
DamoM73 <damomurtagh@gmail.com>
2019-11-29 14:50:47 +1000

    Up to the begining of Part 4

--- api_db.py
+++ api_db.py
@@ -23,6 +23,21 @@ def create_table(filename, tablename, sqlcommand):
         # create empty table
         cursor.execute(sqlcommand)
 
+def table_insert(db_file, table_name, values):
+    # connect to database file
+    with sqlite3.connect(db_file) as database:
+        cursor = database.cursor()
+        
+        # execute an insert statement for each row of provided data list
+        records = 0
+        for row in values:
+            print(row)
+            cursor.execute(f"""
+                            INSERT INTO {table_name}
+                            VALUES ({row});
+                            """)
+            records += 1
+        print(f"Added {records} records to the {table_name} table")
 
 # ---- MAIN PROGRAM ----
 
@@ -58,3 +73,46 @@ create_trucks = """
                 """
 
 create_table(DB_FILE, "Trucks", create_trucks)
+
+# sites table
+create_sites_tbl = """
+                    CREATE TABLE Sites (
+	                    site_id INTEGER PRIMARY KEY,
+	                    street TEXT NOT NULL,
+	                    suburb TEXT NOT NULL,
+	                    postcode INTEGER NOT NULL,
+	                    latitude REAL,
+	                    longitude REAL);
+                    """
+create_table(DB_FILE,"Sites",create_sites_tbl)
+
+# bookings table
+create_bookings_tble = """
+                        CREATE TABLE Bookings (
+                            	booking_id INTEGER PRIMARY KEY,
+                                site_id INTEGER NOT NULL,
+                                truck_id INTEGER NOT NULL,
+                                date TEXT, 
+                                start TEXT,
+                                finish TEXT);
+                        """
+create_table(DB_FILE,"Bookings",create_bookings_tble)
+
+# -- write data from JSON files to database --
+# create a list with an element for each row of the json file
+
+# truck table
+
+values = []
+for row in trucks_data.json():
+    truck_id = row['truck_id']
+    name = row['name']
+    category = row['category']
+    if row['website'] == '':
+        website = 'none'
+    else:
+        website = row['website']
+    
+    values.append(f'{truck_id}, "{name}", "{category}", "{website}"')
+
+table_insert(DB_FILE,"Trucks", values)
